e4fb4f0dd603c3e74ced87af91ad3508ed2d6d3b
everyfine <zuluck2004@icloud.com>
2025-03-17 22:06:32 +0800

    update stock_fund_single_detail_rank_task.py

--- tracker/app/task/stock_fund_single_detail_rank_task.py
+++ tracker/app/task/stock_fund_single_detail_rank_task.py
@@ -19,13 +19,15 @@ from sqlmodel import Session
 
 from app.common.log import log
 from app.core.db import engine
-from app.crud.crud_stock_fund_single_detail_rank import create_stock_fund_single_detail_rank
+from app.crud.crud_stock_fund_single_detail_rank import create_stock_fund_single_detail_rank, \
+    create_stock_fund_single_detail_rank_by_types
 
 
 def execute_create_stock_fund_single_detail_rank():
     log.info(f"{datetime.now()} schedule task [create stock fund single detail rank] start")
     with Session(engine) as session:
         create_count = create_stock_fund_single_detail_rank(session=session)
+        # create_count = create_stock_fund_single_detail_rank_by_types(session, ['10日'])
         log.info(f"{datetime.now()} schedule task [stock fund single detail rank] end, create count: {create_count}")
 
 if __name__ == '__main__':
