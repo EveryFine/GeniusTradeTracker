a3b78166345de6e83449df460c811fc6da38d5f4
everyfine <zuluck2004@icloud.com>
2025-05-05 10:39:05 +0800

    update task to normal

--- tracker/app/crud/crud_stock_history_bao_k_hfq.py
+++ tracker/app/crud/crud_stock_history_bao_k_hfq.py
@@ -35,8 +35,9 @@ def create_stock_history_bao_k_hfq(*, session: Session) -> int:
 
 def create_histories_by_list(session, stock_infos):
     history_count = 0
-    lg = bs.login()
+
     for stock_info in stock_infos:
+        lg = bs.login()
         start_time_stock = time.time()  # 开始计时
         symbol = stock_info.symbol
         exchange = stock_info.exchange
@@ -60,10 +61,12 @@ def create_histories_by_list(session, stock_infos):
 
             log.info(
                 f"history bao k hfq processing data for {code} from {start_date} to {end_date}, total in {time.time() - start_time_stock:.2f} seconds")
+            bs.logout()
         except Exception as e:
             error_msg = f"{datetime.now()} history bao k hfq processing data for {code} from {start_date} to {end_date} error: {str(e)}\n{traceback.format_exc()}"
             log.error(error_msg)
-    bs.logout()
+            bs.logout()
+
     return history_count
 
 
