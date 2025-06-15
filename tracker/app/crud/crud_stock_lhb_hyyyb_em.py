4d14f2dde725f585919ce04bdac2fb6562ce0bb3
everyfine <zuluck2004@icloud.com>
2025-06-15 16:44:10 +0800

    update hyyyb crud end date

--- tracker/app/crud/crud_stock_lhb_hyyyb_em.py
+++ tracker/app/crud/crud_stock_lhb_hyyyb_em.py
@@ -25,7 +25,7 @@ from app.models.stock_lhb_hyyyb_em import StockLhbHyyybEm, StockLhbHyyybEmCreate
 def create_stock_lhb_hyyyb_em(*, session: Session) -> int:
     start_date = get_start_date(session=session)
     # end_date = '20500101'
-    end_date = '20040725'
+    end_date = '20100725'
     count = 0
     stock_lhb_hyyyb_em_df = ak.stock_lhb_hyyyb_em(start_date=start_date, end_date=end_date)
     for index, row in stock_lhb_hyyyb_em_df.iterrows():
